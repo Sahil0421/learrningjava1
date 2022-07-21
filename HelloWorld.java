a14b9369cdf1ab085dfb7d44ecb376cbf642ad63
Sahil0421 <109741599+Sahil0421@users.noreply.github.com>
2022-07-21 18:50:23 +0530

    first commit by Sahil

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World by sahil!");
+ System.out.println("Hello World by sahil");
+   System.out.println("I live in jhungian");
+  System.out.println("I have completed my +2");}
+}
